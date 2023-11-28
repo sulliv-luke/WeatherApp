eb0df7094b5e7013994bd439ed2ed751628d3e34
Luke Sullivan <113471094+sulliv-luke@users.noreply.github.com>
2023-11-28 11:37:09 +0000

    added links to service on render

--- src/api/apiService.jsx
+++ src/api/apiService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/v1/weather';
-const API_FORECAST_URL = 'http://localhost:8080/api/v1/weather/forecast';
+const API_BASE_URL = 'https://weather-service-zv7n.onrender.com/api/v1/weather';
+const API_FORECAST_URL = 'https://weather-service-zv7n.onrender.com/api/v1/weather/forecast';
 
 const apiService = {
   getWeatherNow(endpoint) {
